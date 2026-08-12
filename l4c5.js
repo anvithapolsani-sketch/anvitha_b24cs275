732eb394d2da84ef5265696674019179b7f95e34
anvithapolsani-sketch <anvithapolsani@gmail.com>
2026-08-12 15:26:35 +0530

    Add files via upload

---
+++ l4c5.js
@@ -0,0 +1,9 @@
+const Eventemitter=require("events");
+const e1=new Eventemitter();
+for(let i=1;i<=11;i++){
+    e1.on("message",()=>{
+        console.log(`listener ${i} executed`);
+    });
+
+}
+e1.emit("message");
